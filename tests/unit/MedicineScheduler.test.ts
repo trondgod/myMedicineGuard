8c2c32c23f7aac11553305694967f7ea12852848
Trond Godtland <34607928+trondgod@users.noreply.github.com>
2018-09-10 09:08:38 +0200

    Create MedicineScheduler.test.ts

---
+++ tests/unit/MedicineScheduler.test.ts
@@ -0,0 +1,24 @@
+//import { SomeFile } from '../src/someFile';
+
+describe('x', () => {
+        /** Setup and teardown
+        beforeEach(function() {
+            console.log("beforeEach called"); 
+          });
+        
+        afterEach(function() {
+          });
+    
+        beforeAll(function() {
+          });
+        
+        afterAll(function() {
+          });
+    */
+
+//    it('x', () => {
+//     let expected; 
+//     let actual;
+//     expect(actual).toEqual(expected);
+// });
+});
